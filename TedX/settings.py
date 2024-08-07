d1d1b822d006bc4174a7d4a1803392fa3165ad07
Gboy201 <gursaaz@gmail.com>
2024-08-07 16:50:17 +0200

    Fourtheenth deployment

--- TedX/settings.py
+++ TedX/settings.py
@@ -119,12 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'images/')
-]
+STATIC_ROOT = "/home/Gursaaz/TedX/static"
+
 MEDIA_URl = 'images/'
-MEDIA_ROOT = os.path.join(BASE_DIR)
+MEDIA_ROOT = "/home/Gursaaz/TedX/images"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
